97be36e37e31f56a4d591aa3715151095db8622e
Carrie Koos <carriekoos@Carries-MacBook-Pro.local>
2022-07-03 21:21:32 -0600

    initializing project

---
+++ gladiators/target/debug/deps/gladiators-60ac472eff22f30e.d
@@ -0,0 +1,5 @@
+/Users/carriekoos/Desktop/personal_projects/gladiators/gladiators/target/debug/deps/gladiators-60ac472eff22f30e: src/main.rs
+
+/Users/carriekoos/Desktop/personal_projects/gladiators/gladiators/target/debug/deps/gladiators-60ac472eff22f30e.d: src/main.rs
+
+src/main.rs:
---
+++ gladiators/target/debug/gladiators.d
@@ -0,0 +1,1 @@
+/Users/carriekoos/Desktop/personal_projects/gladiators/gladiators/target/debug/gladiators: /Users/carriekoos/Desktop/personal_projects/gladiators/gladiators/src/main.rs

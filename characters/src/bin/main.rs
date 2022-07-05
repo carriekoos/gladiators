7b050b8dcb7d0c5967835b421444c9e7dd5c915c
Carrie Koos <carriekoos@Carries-MacBook-Pro.local>
2022-07-04 21:01:50 -0600

    initial implementation of Character

--- characters/src/bin/main.rs
+++ characters/src/bin/main.rs
@@ -1,8 +1,60 @@
-use characters_lib::character::{hello_mu, how_old_is_mu};
-// use characters_lib::character::*;
+use characters_lib::character::*;
 
 fn main() {
     hello_mu();
     how_old_is_mu(2);
     println!("Your Walgreens points are ready!");
+    // above is for reference
+
+    let mut some_number: u8 = 15;
+    some_number = 7;
+    println!("my favorite number is: {}", some_number + 1);
+    println!("number of tacos: {}", some_number - 5);
+
+    // make our characters (instances)
+    let mut mu_dog = Character {
+        name: "Mu".to_string(),
+        max_hp: 200,
+        current_hp: 200,
+        level: 2,
+        damage_amount: 110,
+        damage_type: "smelly".to_string(),
+        armor: 0,
+    };
+    let quark_dog = Character {
+        name: "Quark".to_string(),
+        max_hp: 1200,
+        current_hp: 1200,
+        level: 14,
+        damage_amount: 9000,
+        damage_type: "kissy".to_string(),
+        armor: 99,
+    };
+
+
+    // do stuff with our characters
+    mu_dog.whats_my_name(); // this one just prints stuff
+    let some_variable = mu_dog.get_name(); // this is the one that returns
+    println!("{}", some_variable);
+    println!("hi {}", some_variable);
+    println!("hello {}", some_variable);
+    println!("night night {}", some_variable);
+
+    println!("_________________________________________________");
+
+
+    // deal some damage to Mu character
+    println!("Mu has {} hit points.", mu_dog.get_current_hp());
+    mu_dog.receive_damage(1);
+    println!("Mu has {} hit points.", mu_dog.get_current_hp());
+    mu_dog.receive_damage(100);
+    println!("Mu has {} hit points.", mu_dog.get_current_hp());
+
+    // Mu drinks a health potion
+    mu_dog.heal(50);
+    println!("Mu has {} hit points.", mu_dog.get_current_hp());
+
+    // Mu fights a bear (Quark)
+    mu_dog.receive_damage(1000);
+
 }
--- characters/src/character.rs
+++ characters/src/character.rs
@@ -1,9 +1,83 @@
-
-
+/// this is a greeting for the baby dinosaur
 pub fn hello_mu() {
     println!("Hello, Mu!");
 }
 
+/// displays the age of the baby dinosaur
 pub fn how_old_is_mu(age: u8) {
     println!("Mr Mu is {}.", age);
 }
+
+//////////////////////////////////////////////////////////////////
+// above is just for reference
+//////////////////////////////////////////////////////////////////
+
+// here we define the Character struct
+pub struct Character {
+    pub name: String,
+    pub max_hp: u16,
+    pub current_hp: u16,
+    pub level: u16,
+    pub damage_amount: u16,
+    pub damage_type: String,
+    pub armor: u16,
+}
+
+// down here we make all of our functions for the Character struct
+impl Character {
+    /// prints out name
+    pub fn whats_my_name(&self) {
+        println!("My name is {}", self.name);
+    }
+
+    /// returns the name
+    pub fn get_name(&self) -> String {
+        return self.name.clone();
+    }
+
+    /// prints out current hp
+    pub fn whats_my_current_hp(&self) {
+        println!("My current hp is {}", self.current_hp);
+    }
+
+    /// returns current hp
+    pub fn get_current_hp(&self) -> u16 {
+        return self.current_hp;
+    }
+
+    /// lowers hp based on amount of damage
+    pub fn receive_damage(&mut self, amount: u16) {
+
+        // TODO take ARMOR into account here.
+
+        if self.current_hp < amount {
+            self.current_hp = 0;
+        } else {
+            self.current_hp = self.current_hp - amount;
+        }
+
+        if self.current_hp == 0 {
+            println!("Oh no! RIP {}", self.name);
+        }
+    }
+
+    /// heals hp based on amount
+    pub fn heal(&mut self, amount: u16) {
+        // this is the same as below, but harder to read
+        // if self.current_hp + amount > self.max_hp {
+        //     self.current_hp = self.max_hp
+        // } else {
+        //     self.current_hp = self.current_hp + amount
+        // }
+
+
+        let potential_hp = self.current_hp + amount;
+
+        if potential_hp <= self.max_hp {
+            self.current_hp = potential_hp
+        } else {
+            self.current_hp = self.max_hp;
+        }
+    }
+}
+

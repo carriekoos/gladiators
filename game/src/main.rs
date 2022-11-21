2a811622f7d183a06b4bb1207cb7c5c842107399
Matt Koos <mattkoos@gmail.com>
2022-11-20 21:13:20 -0700

    add gladiators with the player

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,69 +1,20 @@
-use bevy::{prelude::*, time::FixedTimestep, window::PresentMode};
+use bevy::{prelude::*, render::texture, time::FixedTimestep, window::PresentMode};
 use leafwing_input_manager::prelude::*;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const ANIMATION_STEP: f32 = 0.15;
 pub const MOVEMENT_STEP: f32 = 1.0 / 60.0; // warning, this is related to PLAYER_SPEED
-pub const PLAYER_SPEED: usize = 4; // warning, this is related to MOVEMENT_STEP
-                                   // pub const MOVEMENT_STEP: f32 = 1.0 / 60.0;
-
-fn main() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        title: "Gladiators".into(),
-                        present_mode: PresentMode::Fifo,
-                        resizable: false,
-                        ..default()
-                    },
-                    ..default()
-                }),
-        )
-        .add_plugin(InputManagerPlugin::<Action>::default())
-        .add_startup_system(setup)
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(MOVEMENT_STEP as f64))
-                .with_system(movement),
-        )
-        .add_system(animate_sprite)
-        .run();
-}
+pub const GLADIATOR_SPEED: usize = 4; // warning, this is related to MOVEMENT_STEP
+pub const GLADIATOR_SIZE: f32 = 4.0; // this scales the size of the sprite - lower once there are many
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
-fn animate_sprite(
-    time: Res<Time>,
-    mut query: Query<
-        (
-            // give me all the entities
-            &mut AnimationTimer,     // with an AnimationTimer,
-            &mut TextureAtlasSprite, // a TextureAtlasSprite,
-            &mut Animation,
-        ),
-        With<Player>,
-    >,
-) {
-    // single_mut() works right now because we only have one Player entity
-    let (mut timer, mut sprite, mut animation) = query.single_mut();
-    timer.tick(time.delta());
-    if timer.just_finished() {
-        sprite.index = animation.get_sprite_index(); // this can change it to 0
-        animation.frame_index += 1; // but then without _seeing_ 0, it gets incremented
-        let (start, end) = animation.animation_type.get_animation_type_indices();
-
-        if animation.frame_index > end - start {
-            animation.frame_index = 0;
-        }
-    }
-}
-
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Gladiator;
 
 #[derive(Component)]
 struct Movement {
@@ -73,108 +24,6 @@ struct Movement {
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
 enum Action {
     Move,
-}
-
-fn movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &Movement, &mut Animation), With<Player>>,
-) {
-    // we know single_mut() works because we're only spawning one player initially.
-    let (mut transform, movement, mut animation) = query.single_mut();
-
-    // get movement input
-    let mut x_movement: i16 = 0;
-    let mut y_movement: i16 = 0;
-
-    if keyboard_input.pressed(KeyCode::W) {
-        y_movement += 1;
-    }
-
-    if keyboard_input.pressed(KeyCode::S) {
-        y_movement -= 1;
-    }
-
-    if keyboard_input.pressed(KeyCode::D) {
-        x_movement += 1;
-    }
-
-    if keyboard_input.pressed(KeyCode::A) {
-        x_movement -= 1;
-    }
-
-    if x_movement == 0 && y_movement == 0 {
-        animation.animation_type = AnimationType::Idle
-    } else {
-        animation.animation_type = AnimationType::Walk
-    }
-
-    // adjust direction
-    animation.animation_direction =
-        match AnimationDirection::from_movement(x_movement.into(), y_movement.into()) {
-            Ok(direction) => direction,
-            Err(err) => {
-                println!(
-                    "Unable to set animation direction. {} Setting to DOWN.",
-                    err
-                );
-                AnimationDirection::Down
-            }
-        };
-
-    // translate
-    let translation_delta: Vec3 =
-        Vec3::new(x_movement.into(), y_movement.into(), 0.0) * movement.speed as f32;
-    transform.translation += translation_delta;
-}
-
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    // spawn camera
-    commands.spawn(Camera2dBundle::default());
-
-    // Note: When spawning an entity, you call commands.spawn() and then chain .insert() over and over,
-    // adding additional components to that entity. Order doesn't matter.
-    // Call spawn() again for a new entity.
-    //
-    // spawn player
-    let texture_handle = asset_server.load("Puny-Characters/Mage-Cyan.png");
-    let texture_atlas = TextureAtlas::from_grid(
-        texture_handle,
-        Vec2::new(24.0, 24.0),
-        24,
-        8,
-        Some(Vec2::new(8.0, 8.0)),
-        None,
-    );
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    commands
-        .spawn((
-            SpriteSheetBundle {
-                texture_atlas: texture_atlas_handle,
-                transform: Transform::from_scale(Vec3::splat(4.0)), // this scales the size of the sprite
-                ..default()
-            },
-            AnimationTimer(Timer::from_seconds(ANIMATION_STEP, TimerMode::Repeating)),
-        ))
-        .insert(Player)
-        .insert(Movement {
-            speed: PLAYER_SPEED,
-        })
-        .insert(Animation {
-            animation_type: AnimationType::Idle,
-            animation_direction: AnimationDirection::Down,
-            frame_index: 0,
-        })
-        .insert(InputManagerBundle::<Action> {
-            action_state: ActionState::default(),
-            input_map: InputMap::default()
-                .insert(DualAxis::left_stick(), Action::Move)
-                .set_gamepad(Gamepad { id: 0 })
-                .build(),
-        });
 }
 
 #[derive(Component)]
@@ -257,6 +106,288 @@ impl AnimationDirection {
             _ => Err(
                 "Movement was not a unit vector and could not generate animation direction.".into(),
             ),
+        }
+    }
+}
+
+/// This is the main function that runs the game.
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Gladiators".into(),
+                        present_mode: PresentMode::Fifo,
+                        resizable: false,
+                        ..default()
+                    },
+                    ..default()
+                }),
+        )
+        .add_plugin(InputManagerPlugin::<Action>::default())
+        .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(MOVEMENT_STEP as f64))
+                .with_system(gladiator_movement)
+                .with_system(player_movement),
+        )
+        .add_system(animate_sprite)
+        .run();
+}
+
+// TODO figure out how to refactor setup into a plugin
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    // spawn camera
+    commands.spawn(Camera2dBundle::default());
+
+    // spawn player
+    spawn_player(
+        Vec2::new(-200.0, -200.0),
+        &mut commands,
+        &asset_server,
+        &mut texture_atlases,
+    );
+
+    //spawn other gladiators
+    const N_GLADIATORS: i32 = 5;
+    for i in 0..N_GLADIATORS {
+        let coordinate = (50 * i) as f32;
+        spawn_gladiator(
+            Vec2::splat(coordinate),
+            i,
+            &mut commands,
+            &asset_server,
+            &mut texture_atlases,
+        );
+    }
+}
+
+fn spawn_player(
+    location: Vec2,
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    texture_atlases: &mut ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("Puny-Characters/Mage-Cyan.png");
+    // The values used in the next function are specific to the Puny Characters sprite sheets
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(24.0, 24.0),
+        24,
+        8,
+        Some(Vec2::new(8.0, 8.0)),
+        None,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    // set size and starting location of the gladiator
+    let mut transform = Transform::from_scale(Vec3::splat(GLADIATOR_SIZE));
+    transform.translation = location.extend(2.0);
+
+    // Note: When spawning an entity, you call commands.spawn() and then chain .insert() over and over,
+    // adding additional components to that entity. Order doesn't matter.
+    // Call spawn() again for a new entity.
+    //
+    // spawn player
+    commands
+        .spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                transform,
+                ..default()
+            },
+            AnimationTimer(Timer::from_seconds(ANIMATION_STEP, TimerMode::Repeating)),
+        ))
+        .insert(Player)
+        .insert(Gladiator)
+        .insert(Movement {
+            speed: GLADIATOR_SPEED,
+        })
+        .insert(Animation {
+            animation_type: AnimationType::Idle,
+            animation_direction: AnimationDirection::Down,
+            frame_index: 0,
+        });
+}
+
+fn spawn_gladiator(
+    location: Vec2,
+    gladiator_idx: i32,
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    texture_atlases: &mut ResMut<Assets<TextureAtlas>>,
+) {
+    let gladiator_sprites = [
+        "Soldier-Blue.png",
+        "Soldier-Red.png",
+        "Soldier-Yellow.png",
+        "Archer-Green.png",
+        "Archer-Purple.png",
+    ];
+    let path = format!(
+        "Puny-Characters/{}",
+        gladiator_sprites[gladiator_idx as usize]
+    );
+    let texture_handle = asset_server.load(&path);
+    // The values used in the next function are specific to the Puny Characters sprite sheets
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(24.0, 24.0),
+        24,
+        8,
+        Some(Vec2::new(8.0, 8.0)),
+        None,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    // set size and starting location of the gladiator
+    let mut transform = Transform::from_scale(Vec3::splat(GLADIATOR_SIZE));
+    transform.translation = location.extend(1.0);
+
+    commands
+        .spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                transform,
+                ..default()
+            },
+            AnimationTimer(Timer::from_seconds(ANIMATION_STEP, TimerMode::Repeating)),
+        ))
+        .insert(Gladiator)
+        .insert(Movement {
+            speed: GLADIATOR_SPEED,
+        })
+        .insert(Animation {
+            animation_type: AnimationType::Idle,
+            animation_direction: AnimationDirection::Down,
+            frame_index: 0,
+        });
+}
+
+fn gladiator_movement(
+    mut query: Query<
+        (&mut Transform, &Movement, &mut Animation),
+        (With<Gladiator>, Without<Player>),
+    >,
+) {
+    for (mut transform, movement, mut animation) in &mut query {
+        animation.animation_type = AnimationType::Walk;
+
+        // maintain either left or right, otherwise default to left
+        let mut x_movement: i16 = -1;
+        (animation.animation_direction, x_movement) = match animation.animation_direction {
+            AnimationDirection::Down => (AnimationDirection::Left, -1),
+            AnimationDirection::DownRight => (AnimationDirection::Left, -1),
+            AnimationDirection::Right => (AnimationDirection::Right, 1),
+            AnimationDirection::UpRight => (AnimationDirection::Left, -1),
+            AnimationDirection::Up => (AnimationDirection::Left, -1),
+            AnimationDirection::UpLeft => (AnimationDirection::Left, -1),
+            AnimationDirection::Left => (AnimationDirection::Left, -1),
+            AnimationDirection::DownLeft => (AnimationDirection::Left, -1),
+        };
+
+        // if too far left, go right
+        if transform.translation[0] < -300.0 {
+            animation.animation_direction = AnimationDirection::Right;
+            x_movement = 1;
+        }
+
+        // if too far right, go left
+        if transform.translation[0] > 300.0 {
+            animation.animation_direction = AnimationDirection::Left;
+            x_movement = -1;
+        }
+
+        // apply the movement
+        let y_movement: i16 = 0;
+        let translation_delta =
+            Vec3::new(x_movement.into(), y_movement.into(), 0.0) * movement.speed as f32;
+        transform.translation += translation_delta;
+    }
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &Movement, &mut Animation), With<Player>>,
+) {
+    // we know single_mut() works because we're only spawning one player right now.
+    let (mut transform, movement, mut animation) = query.single_mut();
+
+    // get movement input
+    let mut x_movement: i16 = 0;
+    let mut y_movement: i16 = 0;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        y_movement += 1;
+    }
+
+    if keyboard_input.pressed(KeyCode::S) {
+        y_movement -= 1;
+    }
+
+    if keyboard_input.pressed(KeyCode::D) {
+        x_movement += 1;
+    }
+
+    if keyboard_input.pressed(KeyCode::A) {
+        x_movement -= 1;
+    }
+
+    if x_movement == 0 && y_movement == 0 {
+        animation.animation_type = AnimationType::Idle
+    } else {
+        animation.animation_type = AnimationType::Walk
+    }
+
+    // adjust direction
+    animation.animation_direction =
+        match AnimationDirection::from_movement(x_movement.into(), y_movement.into()) {
+            Ok(direction) => direction,
+            Err(err) => {
+                println!(
+                    "Unable to set animation direction. {} Setting to DOWN.",
+                    err
+                );
+                AnimationDirection::Down
+            }
+        };
+
+    // translate
+    let translation_delta =
+        Vec3::new(x_movement.into(), y_movement.into(), 0.0) * movement.speed as f32;
+    transform.translation += translation_delta;
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<
+        (
+            // give me all the entities
+            &mut AnimationTimer,     // with an AnimationTimer,
+            &mut TextureAtlasSprite, // a TextureAtlasSprite,
+            &mut Animation,
+        ),
+        With<Gladiator>,
+    >,
+) {
+    // single_mut() works right now because we only have one Player entity
+    for (mut timer, mut sprite, mut animation) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = animation.get_sprite_index(); // this can change it to 0
+            animation.frame_index += 1; // but then without _seeing_ 0, it gets incremented
+            let (start, end) = animation.animation_type.get_animation_type_indices();
+
+            if animation.frame_index > end - start {
+                animation.frame_index = 0;
+            }
         }
     }
 }

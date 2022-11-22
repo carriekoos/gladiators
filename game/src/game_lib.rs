9b1ac766ee4324a934868d9edac899490b01466c
Matt Koos <mattkoos@gmail.com>
2022-11-21 20:08:44 -0700

    jotted down some design thoughts for the grid

--- game/src/game_lib.rs
+++ game/src/game_lib.rs
@@ -11,7 +11,11 @@ pub mod player;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 
-/// G ame window constants
+/// Grid constants
+pub const HORIZONTAL_DIVISIONS: f32 = 20.; // vertical divisions calculated by this * (window) height/width
+pub const GRID_EVALUATION_STEP: f32 = 1.0 / 30.0;
+
+/// Game window constants
 pub const WINDOW_WIDTH: f32 = 1280.;
 pub const WINDOW_HEIGHT: f32 = 720.;
 pub const BACKGROUND_WIDTH: f32 = 6020.;
@@ -26,7 +30,7 @@ pub const PLAYER_START_X: f32 = -300.0;
 pub const PLAYER_START_Y: f32 = -200.0;
 
 /// Gladiator constants
-pub const MOVEMENT_STEP: f32 = 1.0 / 60.0; // warning, this is related to PLAYER_SPEED
+pub const MOVEMENT_STEP: f32 = 1.0 / 60.0; // warning, this is related to GLADIATOR_SPEED
 pub const N_GLADIATORS: i32 = 5;
 pub const GLADIATOR_SPEED: usize = 4; // warning, this is related to MOVEMENT_STEP
 pub const GLADIATOR_SIZE: f32 = 2.5; // this scales the size of the sprite - lower once there are many
@@ -41,7 +45,9 @@ pub const GLADIATOR_SPRITES: [&str; 5] = [
 ];
 
 #[derive(Component)]
-pub struct Engaged;
+pub struct CombatStatus {
+    engaged: bool,
+}
 
 #[derive(Component)]
 pub struct Unengaged;
--- game/src/grid.rs
+++ game/src/grid.rs
@@ -1,1 +1,68 @@
+use std::collections::HashMap;
 
+use bevy::{prelude::*, time::FixedTimestep};
+
+use crate::{animation::*, gladiator::*, *};
+
+pub struct GridPlugin;
+
+impl Plugin for GridPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_grid).add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(GRID_EVALUATION_STEP as f64))
+                .with_system(evaluate_grid),
+        );
+    }
+}
+
+/// Spawns the grid
+fn spawn_grid() {}
+
+fn evaluate_grid() {}
+
+// TODO I think the grid is a resource, not a component
+
+pub struct GridLocation {
+    x: i32,
+    y: i32,
+}
+
+/// ArenaGrid stores a Vec of Entitys that are located at each
+/// GridLocation, which is a section of the whole arena.
+/// The HashMap could either be reconstructed at a given rate or
+/// it could be optimized to only be updated by moving Gladiators,
+/// but that seems like a really complicated option. Initial impl
+/// will be to reconstruct by looping over all Gladiators at about
+/// half the interval as the MOVEMENT_STEPs are.
+/// This is likely a performance bottleneck, but can be optimized
+/// at a later time.
+///
+/// Possible option: Gladiators can ask for their current grid
+/// location and store their previous grid location. On change,
+/// an Event can be emitted that the ArenaGrid is listening for,
+/// which hopefully can have the Entity and location data in it.
+/// It would need previous coordinates as well as new, so that
+/// it could be removed from the previous GridLocation key.
+/// Gladiators may getting their current GridLocation anyways
+/// for the purposes of figuring out what other Gladiators
+/// are nearby so they can know where to move. Bit of a twofer
+/// so this may be a pretty sensible option and allows for
+/// carving out a chunkier grid as a performance lever.
+///
+/// I think minimizing the mutable reference to this guy
+/// will be a performance win. I think that can be accomplished
+/// by the events so that the Grid can mutate itself.
+///
+/// Gladiators will need to know their GridLocation and two
+/// other things:
+/// 1. Entity of Gladiator(unengaged) in the same GridLocation. (can engage)
+/// 2. Entities of Gladiators(unengaged) in any of the adjacent
+///  GridLocations. (will move towards)
+pub struct ArenaGrid {
+    grid_map: HashMap<GridLocation, Vec<Entity>>,
+    // https://docs.rs/bevy/latest/bevy/prelude/struct.Query.html#method.get
+    // Store the Entity in this hashmap. Then any query that would contain
+    // this Entity, can just use query.get(Entity) instead of looping through
+    // the entire query.
+}
